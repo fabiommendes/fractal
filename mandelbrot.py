389cb201b51c353a970d4b97d238ec23bdcfdb13
Fábio Macêdo Mendes <fabiomacedomendes@gmail.com>
2021-09-22 11:21:13 -0300

    Mapeia coordenadas i j no plano complexo

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,4 +1,8 @@
 import pyxel
+from random import choice, randint, seed, random
+
+# ...
+pixels = [[False] * 256 for _ in range(256)]
 
 
 def update():
@@ -7,10 +11,31 @@ def update():
 
 def draw():
     pyxel.cls(pyxel.COLOR_WHITE)
+    # seed(0)
 
+    # Itera sobre todos os pixels da tela, pintando de cores
+    # aleatórias
+    for i in range(pyxel.mouse_x, pyxel.mouse_x + 1):
+       for j in range(pyxel.mouse_y, pyxel.mouse_y + 1):
+           pyxel.pset(i, j, mandelbrot_color(i, j))
+
+    # for i in range(0, 256):
+    #     for j in range(256):
+    #         pyxel.pset(i, j, mandelbrot_color(i, j))
+
+
+def mandelbrot_color(i, j):
+    z = get_complex(i, j)
+    if pyxel.btnp(pyxel.KEY_SPACE):
+        print(z, abs(z))
+    ... # iteração de mandelbrot
+    ... # decide a cor a ser utilizada 
+    return randint(0, 15)
 
 def get_complex(i, j):
-    return i + 1j * j
+    re = +i / 64 - 2
+    im = -j / 64 + 2
+    return complex(re, im)
 
 
 pyxel.init(256, 256)
